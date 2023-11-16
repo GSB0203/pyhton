f20532d0774a59c42b15fcfaa48a8c83936c90d8
gimseungbin0203 <gimseungbin0203@gmail.com>
2023-11-16 09:27:34 +0900

    2

--- 박민하쌤/원형큐 adt.py
+++ 박민하쌤/원형큐 adt.py
@@ -4,46 +4,57 @@ from typing import Self
 class CircularQueue:
   def __init__(self, capacity = 5):
     self.capacity = capacity
-    self.list = [None] * capacity
+    self.list = [None] * (capacity)
     self.front = 0
     self.rear = 0
 
-def isEmpty(self):
-  return self.front == self.rear
+  def isEmpty(self):
+    return self.front == self.rear
 
-def isFull(self):
-  return self.rear == self.capacity - 1
+  def isFull(self):
+    return (self.rear + 1) % self.capacity == self.front
 
-def enqueue(self, item):
-  if(isFull(self)):
-    print("큐가 가득 찼습니다.")
+  def enqueue(self, item):
+    if(self.isFull()):
+      print("큐가 가득 찼습니다.")
   
-  else:
-    self.rear = (self.rear + 1) %self.capacity
-    self.list[self.rear] = item
+    else:
+      self.rear = (self.rear + 1) % self.capacity
+      self.list[self.rear] = item
 
-def dequeue(self):
-  if(isEmpty(self)):
-    print("큐가 비어있습니다.")
+  def dequeue(self):
+    if(self.isEmpty()):
+      print("큐가 비어있습니다.")
 
-  else :
-    self.front = (self.front + 1) % self.capacity
-    print(self.list[self.front])
+    else :
+      self.front = (self.front + 1) % self.capacity
+      print (self.list[self.front])
 
 
-def peek(self):
-  if(isEmpty(self)):
-    print("큐가 비어있습니다.")
-  else:
-    print(self.list[self.rear])
+  def peek(self):
+    if(self.isEmpty()):
+      print("큐가 비어있습니다.")
+    else:
+      print(self.list[self.rear])
+
 
 
 queue = CircularQueue()
+queue.peek()
 queue.enqueue(1)
+queue.peek()
 queue.enqueue(2)
+queue.peek()
 queue.enqueue(3)
+queue.peek()
 queue.enqueue(4)
+queue.peek()
+print(queue.list)
 queue.enqueue(5)
+queue.peek()
 queue.dequeue()
-queue.dequeue()
+print(queue.list)
 queue.enqueue(6)
+queue.peek()
+
+print(queue.list)
