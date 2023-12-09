b29f6b87f4e0df1bc5f883154f9d36a4d2ecceff
gimseungbin0203 <gimseungbin0203@gmail.com>
2023-12-09 22:35:03 +0900

    ADD:: 선형큐 & 선형덱

--- 박민하쌤/선형덱 adt.py
+++ 박민하쌤/선형덱 adt.py
@@ -1,81 +1,67 @@
-#미완성
-class LinearDeque:
-    def __init__(self, capacity=5):
-        self.capacity = capacity
-        self.lineardeque = [None] * capacity
-        self.front = -1 
-        self.rear = -1  
+class Deque:
+    def __init__(self, capacity):
+        self.capacity = capacity  # 덱 최대 사이즈
+        self.list = [None] * capacity  # 덱 요소 저장 리스트
+        self.front = 0  # 덱 앞부분 가르키는 인덱스 & 초기화
+        self.rear = 0   # 덱 뒷부분을 가리키는 인덱스 & 초기화
 
-    def is_empty(self):
-        return self.front == self.rear
+    
+    def isEmpty(self):  #덱이 비었는지 확인하는 함수
+        return self.front == self.rear  #front와 rear가 같은가? true, 아니면 false
+    
+    def isFull(self):  #덱이 가득 찼는지 확인하는 함수
+        return self.front == (self.rear+1)%self.capacity  #front와 그 다음 채울 공간이 가득 찼는가? true, 아니면 false
+    
+    def addFront(self, e):  #front에서 값 삽입
+        if not self.isFull():  #가득 차지 않았는가?
+            self.list[self.rear] = e  #현재 rear 위치에 입력받은 값 삽입
+            self.rear += 1  #rear값 증가
+        else:  #가득 찼다면
+            print("덱이 가득 찼습니다.")  #출력
+    
+    def deleteFront(self):  #front에서 값 제거
+        if not self.isEmpty():  #비어있지 않은가?
+            self.rear -= 1  #rear를 하나 낮춤
+            item =  self.list[self.rear]  #삭제할 값을 item 저장
+            self.list[self.rear] = None  #값이 있던 공간 None 저장
+            print(item)  #제거 값 출력
+            return item  #제거된 요소 리턴
+        else:  #비었다면
+            print("덱이 비어있습니다.")  #출력
+    
+    def addRear(self, e):  #rear에서 값 삽입
+        if not self.isFull():  #가득 차지 않았다면
+            self.front -= 1  #front값 낮추고
+            self.list[self.front] = e  #현재 front 위치에 입력받은 값 삽입
+        else:  #가득찼다면
+            print("덱이 가득 찼습니다.")  #출력
 
-    def is_full(self):
-        return self.rear == self.capacity - 1
-
-    def addrear(self, item):
-        if self.is_full():
-            print("덱이 가득 찼습니다.")
-        else:
-            self.rear += 1
-            self.lineardeque[self.rear] = item
-
-    def addfront(self, item):
-        if self.is_full():
-          print("덱이 가득 찼습니다.")  
-        else:
-            
-          
-
-    def deleterear(self):
-        if self.is_empty():
-            print("덱이 비어있습니다.")
-        else:
-            self.lineardeque[self.front] = None
+    def deleteRear(self):  #rear에서 값 제거
+        if not self.isEmpty():  #비어있지 않다면
+            item =  self.list[self.front]  #삭제할 값을 item 저장
+            self.list[self.front] = None  #값이 있던 공간 None 저장
+            self.front +=  1  #front 다음 공간 지정
+            print(item)  #제거된 값 출력
+            return item  #제거된 요소 리턴
+        else:  #비었다면
+            print("덱이 비어있습니다.")  #출력
 
 
-    def deletefront(self):
-        if self.is_empty():
-            print("덱이 비어있습니다.")
-            return None
-        else:
-            item = self.lineardeque[self.front]
-            if self.front == self.rear:
-                self.front = -1
-                self.rear = -1
-            else:
-                self.front += 1
-            return item
+queue = Deque(5)
 
-    def getfront(self):
-        if self.is_empty():
-            print("큐가 비어있습니다.")
-            return None
-        else:
-            return self.lineardeque[self.front]
-         
-LinearDeque = LinearDeque()
-print(LinearDeque.getfront()) 
+queue.addFront(1)
+queue.addFront(2)
+queue.addRear(3)
+queue.addRear(4)
+queue.addRear(5)
 
-LinearDeque.addrear(1)
-print(LinearDeque.getfront())
 
-LinearDeque.addrear(2)
-print(LinearDeque.getfront())  
+print(queue.list)
 
-LinearDeque.addrear(3)
-print(LinearDeque.getfront())
+queue.deleteRear()
 
-LinearDeque.addrear(4)
-print(LinearDeque.getfront()) 
+print(queue.list)
 
-LinearDeque.addrear(5)
-print(LinearDeque.getfront())
+queue.deleteFront()
 
-LinearDeque.addrear(6)
-print(LinearDeque.getfront())
-
-print(LinearDeque.lineardeque)
-
-LinearDeque.deletefront()
-print(LinearDeque.getfront())
-
+print(queue.list)
--- 박민하쌤/선형큐 adt.py
+++ 박민하쌤/선형큐 adt.py
@@ -3,43 +3,42 @@
 from typing import Self
 
 class CircularQueue:
-  def __init__(self, capacity = 5):
-    self.capacity = capacity
-    self.list = [None] * (capacity)
-    self.front = -1
-    self.rear = -1
+  def __init__(self, capacity = 5):  #큐 초기화 함수
+    self.capacity = capacity  #큐 최대 사이즈
+    self.list = [None] * (capacity)  # 큐 요소 저장 리스트
+    self.front = -1  #큐 앞부분 가르키는 인덱스 & 초기화
+    self.rear = -1  #큐 뒷부분 가르키는 인덱스 & 초기화
 
-  def isEmpty(self):
-    return self.front == self.rear
+  def isEmpty(self):  #큐가 비었는지 확인하는 함수
+    return self.front == self.rear  #큐가 비었으면 true, 아니면 false
 
-  def isFull(self):
-    return self.rear == self.capacity - 1
+  def isFull(self):  #큐가 가득 찼는지 확인하는 함수
+    return self.rear == self.capacity - 1  #큐가 가득 찼으면 true, 아니면 false
 
-  def enQueue(self, item):
-    if(self.isFull()):
-      print("큐가 가득 찼습니다.")
-      pass
-    else:
-      self.rear += 1
-      self.list[self.rear] = item
+  def enQueue(self, item):  #값 삽입
+    if(self.isFull()):  #큐가 가득 찼는가?
+      print("큐가 가득 찼습니다.")  #출력
+    else:  #아니면
+      self.rear += 1  #rear 값 증가
+      self.list[self.rear] = item  #rear 위치에 값 저장
 
-  def deQueue(self):
-    if(self.isEmpty()):
-      print("큐가 비어있습니다.")
-      pass
-    else :
-      self.front += 1
-      print(self.list[self.front])
-      return self.list[self.front]
+  def deQueue(self):  #값 제거
+    if(self.isEmpty()):  #큐가 비었는가?
+      print("큐가 비어있습니다.")  #출력
+    else :  #아니면
+      self.front += 1  #front 값 증가
+      item = self.list[self.front]  #삭제할 값 itme에 저장
+      self.list[self.front] = None  #값이 있던 공간 None 저장
+      print(item)  #제거 값 출력
+      return item  #제거 값 리턴
 
 
-  def peek(self):
-    if(self.isEmpty()):
-      print("큐가 비어있습니다.")
-      pass
-    else:
-      print(self.list[self.front])
-      return self.list[self.front]
+  def peek(self):  #가장 처음 값 보기
+    if(self.isEmpty()):  #큐가 비었는가?
+      print("큐가 비어있습니다.")  #출력
+    else:  #아니면
+      print(self.list[self.front])  #가장 처음에 있던 값 출력
+      return self.list[self.front]  #가장 처음에 있던 값 리턴
 
 
 
@@ -47,13 +46,9 @@ class CircularQueue:
 queue = CircularQueue()
 queue.peek()
 queue.enQueue(1)
-queue.peek()
 queue.enQueue(2)
-queue.peek()
 queue.enQueue(3)
-queue.peek()
 queue.enQueue(4)
-queue.peek()
 print(queue.list)
 queue.enQueue(5)
 queue.peek()
@@ -61,5 +56,6 @@ queue.deQueue()
 print(queue.list)
 queue.enQueue(6)
 queue.peek()
+queue.deQueue()
 
 print(queue.list)
